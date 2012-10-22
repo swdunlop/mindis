b8bc5b78dd337ab6ac9ed7675ceb0d8e07ba6276
Scott Dunlop <swdunlop@gmail.com>
2012-10-21 21:37:12 -0700

    fixed association of documentation with errors.go

--- errors.go
+++ errors.go
@@ -1,12 +1,15 @@
 package mindis
 
 /* A simple Error abstraction using constant strings. */
-
 type Error string
 
+/* An implementation of the "error" interface. */
 func (e Error) Error() string {
 	return string(e)
 }
 
+/* Produced when an invalid type is passed to either Scan, Exec or Send. */
 const TYPE_ERROR = Error("unsupported data type encountered")
+
+/* Produced when an invalid response is read from the server. */
 const PROTOCOL_ERROR = Error("an invalid response was read from the server")
